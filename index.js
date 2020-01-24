01544cc7696680e4905a60648a0ee0bbd7fccfba
trogulja <tibor.rogulja@tiskarazagreb.hr>
2020-01-24 23:00:24 +0100

    newRb

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const {
 const spreadsheetId = process.env.SHEET_ID;
 const sheetName = process.env.SHEET_RANGE;
 
+function pad(n, width) {
+  n = n + "";
+  return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
+}
+
 async function readSheets() {
   const auth = await getAuthToken();
   const res = await getSpreadSheetValues({
@@ -19,19 +24,30 @@ async function readSheets() {
     auth
   });
 
-  let sheetData = [];
-
   try {
     if (res.data.values.length) {
       // Handle last entry
-      sheetData.push(res.data.values[res.data.values.length - 1]);
+      let rb = res.data.values[res.data.values.length - 1][0];
+      let rbYear = Number(rb.toString().substring(0, 2));
+      let rbNums = Number(rb.toString().substring(2));
+
+      let newYear = new Date()
+        .getFullYear()
+        .toString()
+        .substring(2);
+      let newRb = newYear + "001"; // default value YYnnn
+
+      if (Number(newYear) === rbYear) newRb = "" + rbYear + pad(rbNums + 1, 3);
+
+      return newRb;
     } else {
       console.log("No data found.");
+      return false;
     }
   } catch (err) {
     console.log("The API returned an error:", err);
+    return false;
   }
-  return sheetData;
 }
 
 readSheets().then(console.log);
